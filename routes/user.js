a63273c39f7e49666bfc31b54a2d926f05f6e46e
Jonathan Leverenz <jonlev03@gmail.com>
2022-06-18 21:30:03 -0400

    updated project misc

---
+++ routes/user.js
@@ -0,0 +1,8 @@
+const router = require("express").Router();
+
+router.get("/usertest", (req, res) => {
+  res.send("user test is successful");
+});
+
+lh: 3000 / api / user / usertest;
+module.exports = router;
