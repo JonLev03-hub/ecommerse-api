8ef0f9958a86bb37d8fb6f37bc5899a185d81892
Jonathan Leverenz <jonlev03@gmail.com>
2022-07-01 02:08:45 -0400

    minor bug fixes

--- index.js
+++ index.js
@@ -26,6 +26,6 @@ app.use("/api/product", productRoute);
 app.use("/api/cart", cartRoute);
 app.use("/api/order", orderRoute);
 
-app.listen(process.env.PORT || 3000, () => {
-  console.log("Server is running on port 3000");
+app.listen(process.env.PORT || 5000, () => {
+  console.log("Server is running on port 5000");
 });
--- models/Product.js
+++ models/Product.js
@@ -4,10 +4,11 @@ const ProductSchema = new mongoose.Schema(
     title: { type: String, required: true, unique: true },
     desc: { type: String, required: true },
     img: { type: String, required: true },
-    color: { type: String },
-    size: { type: String },
+    color: { type: Array },
+    size: { type: Array },
     category: { type: Array, required: true },
     price: { type: Number, required: true },
+    inStock: { type: Boolean, default: true },
   },
   { timestamps: true }
 );
