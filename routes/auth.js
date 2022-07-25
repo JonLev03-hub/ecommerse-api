3ed257619d1abac90de27af924bb7106e06a7904
jonlev03-hub <jonlev03@gmail.com>
2022-07-25 02:55:56 -0400

    fixed failed authentication

--- routes/auth.js
+++ routes/auth.js
@@ -49,7 +49,7 @@ router.post("/login", async (req, res) => {
       res.status(200).json({ ...others, accessToken });
     }
   } catch (err) {
-    res.status(500).json(err);
+    res.status(500)
   }
 });
 
